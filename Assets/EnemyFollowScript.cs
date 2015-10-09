b2b0c01a64b9cc62daf3b74b2a90cceb4995956a
crazymeeshu <crazymeeshu@gmail.com>
2015-10-08 21:22:13 -0700

    Created git repo

---
+++ Assets/EnemyFollowScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFollowScript : MonoBehaviour {
+
+	public GameObject Player;
+	public float moveSpeed = 4.0f;
+	public int rotationSpeed = 3;
+
+	private Vector2 target;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		target = Player.transform.position;
+		transform.position = Vector2.MoveTowards (transform.position, target, moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/TextControlScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextControlScript : MonoBehaviour {
+	
+	public string stringToEdit = "";
+	public Transform ball;
+	public Transform cube;
+	public Transform cap;
+	bool hasReturned = false;
+	bool castUp = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasReturned == true){
+			if(stringToEdit.ToLower() == "ball"){
+				Transform newball = (Transform)Instantiate (ball, new Vector3(0, 0, 0), Quaternion.identity);
+			}
+			else if (stringToEdit.ToLower() == "cube"){
+				Transform newcube = (Transform)Instantiate (cube, new Vector3(0, 0, 0), Quaternion.identity);
+			}
+			else if (stringToEdit.ToLower () == "cap"){
+				Transform newcap = (Transform)Instantiate (cap, new Vector3(0, 0, 0), Quaternion.identity);
+			}
+			stringToEdit = "";
+		}
+	}
+	
+	void OnGUI() {
+		GUI.SetNextControlName ("Spellbook");
+		Event e = Event.current;
+		if (e.keyCode == KeyCode.Return){
+			hasReturned = true;
+			castUp = false;
+		}
+		else if (e.keyCode == KeyCode.Alpha1){
+			castUp = true;
+			hasReturned = false;
+			stringToEdit = "";
+			GUI.FocusControl("Spellbook");
+		}
+		else if (hasReturned == false && castUp == true)
+			stringToEdit = GUI.TextField(new Rect(10, 10, 200, 20), stringToEdit, 25);
+	}
+}
