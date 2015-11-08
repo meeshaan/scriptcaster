97dd7804d6fa952e7bc282cea17c3adca4afcba9
Haub100 <haub100@mail.chapman.edu>
2015-11-08 11:40:31 -0800

    Enemy Path Following
    FollowPath and PathDefinition are the only scripts you need to make
    something follow a path (doesn't have to be an enemy). WaterEleFacing
    takes any game object (i used it specifically for the water elemental)
    and compares its x position to the x position of the path checkpoint it
    is headed towards and decides whether it should be facing left or right.

---
+++ Assets/Scripts/WaterEleFacing.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaterEleFacing : MonoBehaviour {
+
+  bool facingLeft = true;
+  //public FollowPath path;
+  //public IEnumerator<Transform> _currentPoint;
+  public GameObject WaterEle;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    //_currentPoint = path._currentPoint;
+    Vector3 PointPos = WaterEle.GetComponent<FollowPath>()._currentPoint.Current.position;
+    Vector3 ElementalPos = WaterEle.transform.position;
+
+    if(PointPos.x < ElementalPos.x && !facingLeft)
+    {
+      Flip();
+    }
+    else if (PointPos.x > ElementalPos.x && facingLeft)
+    {
+      Flip();
+    }
+	}
+
+  void Flip()
+  {
+    facingLeft = !facingLeft;
+    Vector3 theScale = transform.localScale;
+    theScale.x *= -1;
+    transform.localScale = theScale;
+  }
+}
