15b9511962e7b0343d406a0c4409aa35ebbc4c1d
Andy de Caussin <decau101@mail.chapman.edu>
2015-11-19 16:53:11 -0800

    Added Air enemy animations and placed boundaries into game
    Thinking about increasing the size of the level to accommodate  more
    enemies and a larger level.

---
+++ Assets/Scripts/bottomBoundaryCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bottomBoundaryCollision : MonoBehaviour {
+	GameObject player;
+	public int x = 0;
+	public int y = 1;
+	public Vector2 start = new Vector2(0,1);
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		player.transform.position = start;
+	}
+}
