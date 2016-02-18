29ae47862520e42bbc1bc0b928b8214be3584643
abenjamin13 <abenjamin13@aol.com>
2016-02-17 17:04:00 -0800

    Basic flying enemy ai
    Still working on it but wanted to commit now

---
+++ Assets/PlayerSpot.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpot : MonoBehaviour {
+		
+	public Transform startSight, endSight;
+	public bool spotted = false;
+	public bool facingLeft = true;
+	public GameObject Player;
+	public Vector2 target;
+	public float moveSpeed = 1.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Patrol", 0f, Random.Range(4f,6f)) ;
+		Player = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Raycasting ();
+		Behaviors ();
+		movement ();
+
+	}
+
+	void Raycasting()
+	{
+		Debug.DrawLine (startSight.position,endSight.position,Color.red);
+		spotted = Physics2D.Linecast (startSight.position, endSight.position, 1 << LayerMask.NameToLayer("Player"));
+
+	}
+
+	void Behaviors()
+	{
+
+	}
+	void Patrol ()
+	{
+
+		//flip left and right at random intervals
+		facingLeft = !facingLeft;
+		if (facingLeft == true) {
+			transform.eulerAngles = new Vector2 (0, 0);
+		}
+		else {
+			transform.eulerAngles = new Vector2 (0, 180);
+		}
+	}
+	void movement()
+	{
+		if (spotted == true) {
+			target = Player.transform.position;
+			transform.position = Vector2.MoveTowards (transform.position, target, moveSpeed * Time.deltaTime);
+		}
+	}
+}
