ec8b9d80914f5f2c52a6eba93d1503a5b30f7094
Haub100 <haub100@mail.chapman.edu>
2016-02-17 21:51:51 -0800

    Added VSCode Functionality and the beginnings of enemy ground AI

---
+++ Assets/Scripts/AI/AI_Patrol.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Patrol : MonoBehaviour {
+    
+    /*
+    Default values:
+        -patrolDistance: 2.5
+        -patrolSpeed: 10
+        -pursuitSpeed: 15
+        -inPursuit: false
+        -facingLeft: true
+    */
+    
+    public float patrolDistance;                //how large the enemy patrol distance is
+    public float patrolSpeed;                   //how fast the enemy moves while patrolling
+    public float pursuitSpeed;                  //how fast the enemy move while in pursuitSpeed
+    public Transform groundChecker;             //Gameobject required, placed where you wish "ground" to be detected from
+    public Transform blockChecker;              //Gameobject required, placed where you wish the side collision detector to be (works similar to groundchecker)
+    public bool isGrounded = false;             //Check to see if we are grounded
+    public bool isBlocked = false;              //Check to see if we are blocked
+    public bool inPursuit = false;              //When in patrol-mode this remains false
+    public bool facingLeft = true;              //Keeps track of which direction the enemy is facing
+    public bool detectPlayer = false;
+
+	private float patrolStartingX;
+    private bool movingLeft = true;             //Keeps track of which drirection the enemy should be moving
+    private bool hitground = false;             //Turns true when the enemy hits the ground
+    
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+	    //Casts a line between our ground checker gameobject and our player
+        //If the floor is between us and the groundchecker, this makes "isGrounded" true
+        isGrounded = Physics2D.Linecast(transform.position, groundChecker.position, 1 << LayerMask.NameToLayer("Platform"));
+        isBlocked = Physics2D.Linecast(transform.position, blockChecker.position, 1 << LayerMask.NameToLayer("Platform"));
+        //isBlocked = Physics2D.Linecast(transform.position, blockChecker.position, 1 << LayerMask.NameToLayer("Player"));
+        
+        //If we are grounded and not in pursuit run the patrol script
+        if(isGrounded && !inPursuit){
+          Patrol();  
+        }
+
+	}
+    
+    //When the enemy lands after being in the air this keeps track of the start of the patrol
+    void OnCollisionEnter2D(Collision2D coll){
+        if(coll.gameObject.layer == 9 && !isGrounded){
+            patrolStartingX = transform.position.x;
+        }
+    }
+    
+    //Handles the movement of the enemy when patrolling a sector
+    void Patrol(){
+        //When moveingLeft == true then we run this script to move left otherwise we move right
+        if(movingLeft){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-1 * 10 * patrolSpeed * Time.deltaTime, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else{
+            GetComponent<Rigidbody2D>().velocity = new Vector2(1 * 10 * patrolSpeed * Time.deltaTime, GetComponent<Rigidbody2D>().velocity.y);           
+        }
+        
+        //Checks whether we should be moving left or right based on the patrol distance or by being blocked
+        if((patrolStartingX - patrolDistance) > transform.position.x || isBlocked && movingLeft){
+            movingLeft = false;
+        }
+        else if((patrolStartingX + patrolDistance) < transform.position.x || isBlocked && !movingLeft){
+            movingLeft = true;
+        }
+
+        //checks which direction the enemy is moving and flips it to the correct side
+        if (movingLeft && !facingLeft)
+        {
+            Flip();
+        }
+        else if (!movingLeft && facingLeft)
+        {
+            Flip();
+        }        
+    }
+    
+    void Flip(){
+        facingLeft = !facingLeft;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/AI/AI_PlayerDetection.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_PlayerDetection : MonoBehaviour {
+
+    public Transform sightStart;
+    public Transform sightEnd;
+    public float startingAngle;
+    public float arcDegrees;
+    public float timePeriod;
+    public bool isDetected = false;
+    public float radius;
+    
+    private float _Time;
+
+    
+    private bool sweepingUp = true;
+   
+    
+    // Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Gizmos.DrawSphere(sightStart.position, radius);
+        Physics2D.CircleCast(sightStart.position, 10,);
+        Raycasting();
+        Behaviors();
+	}
+    
+    void Raycasting(){
+        if(!isDetected) {
+            visionSweep();
+        }
+        else{
+            detectionBubble();
+        }
+    }
+    
+    void Behaviors(){
+        
+    }
+    
+    //Function that creates a linecast and rotates the sight line empty game object.
+    //By doing this we rotate the vision linecast in arc to detect the player.
+    void visionSweep(){
+        Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
+        isDetected = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+        
+        _Time = _Time + Time.deltaTime;
+        float phase = Mathf.Sin(_Time / timePeriod);
+        transform.localRotation = Quaternion.Euler( new Vector3(0, 0, (phase * arcDegrees) + -startingAngle));
+    }
+    
+    void detectionBubble(){
+        
+    }
+}
