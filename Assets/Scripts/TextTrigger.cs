9750ada13ecb7e70340b5f425aa75bef121a24ab
Alexander Sacharske <sacha101@mail.chapman.edu>
2016-03-10 20:29:27 -0800

    Textbox

---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTrigger : MonoBehaviour {
+
+    public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag != "Player")
+        {
+            Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), c.gameObject.GetComponent<Collider2D>());
+        }
+        else if (c.gameObject.tag == "Player" && rend.enabled == false)
+        {
+        Debug.Log("displaytext");
+            rend.enabled = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (rend.enabled)
+        {
+            rend.enabled = false;
+        }
+    }
+}
