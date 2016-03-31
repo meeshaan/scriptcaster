35813b22fe26c1b4ad0267cb8991025b215ececc
abenjamin13 <abenjamin13@aol.com>
2016-03-31 12:53:01 -0700

    ai for boss 2

---
+++ Assets/Scripts/Boss2Script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss2Script : MonoBehaviour {
+    public GameObject Player;
+    public float moveSpeed;
+   
+    private Vector2 target;
+    
+
+	// Use this for initialization
+	void Start () {
+	   Player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target = Player.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed*Time.deltaTime);
+	
+	}
+}
